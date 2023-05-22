7bb3c1c569ded567332a71ad652786be4b1b71c5
JMQuehl <jannik.quehl@gmail.com>
2023-05-22 23:08:17 +0200

    Forgot adding test for task 02

---
+++ tests/test_task02.py
@@ -0,0 +1,16 @@
+from tests.abstract_test import TaskTest
+import unittest
+from advent_of_code_2021.tasks.task02.task02 import Task02
+from advent_of_code_2021.advent_of_code_utils import parse_args
+
+
+class Task01Tests(TaskTest, unittest.TestCase):
+    task = Task02(parse_args([]))
+    known_input = ["forward 5\n",
+                   "down 5\n",
+                   "forward 8\n",
+                   "up 3\n",
+                   "down 8\n",
+                   "forward 2\n"]
+    known_output = 150
+    known_bonus_output = 900
